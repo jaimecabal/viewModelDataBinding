e8895be68caa1e661f71b6b7ec5029f320e88d26
jcabal <jcabalf3@gmail.com>
2022-03-08 18:31:47 +0100

    Navigation y la pila de actividades

--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -22,6 +22,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentPickupBinding
 import com.example.cupcake.model.OrderViewModel
 
@@ -70,4 +71,9 @@ class PickupFragment : Fragment() {
         super.onDestroyView()
         binding = null
     }
+    fun cancelOrder() {
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_pickupFragment_to_startFragment)
+    }
+
 }
